041b5486317b56b6968d1fddd27e3cac0d75d394
rafayet-monon <rafayet.monon@gmail.com>
2021-09-25 17:06:03 +0600

    [#4] Restructure VCR config

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,10 +4,11 @@ require 'vcr'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr'
+  c.allow_http_connections_when_no_cassette = false
   c.hook_into :webmock
-  c.configure_rspec_metadata!
   c.ignore_localhost = true
   c.ignore_request do |request|
     URI(request.uri).port == 9200
   end
+  c.default_cassette_options = { record: :none, match_requests_on: [:path] }
 end
